2d9e267e91ba80c769bac9148c5ecf9742aa3423
Leo <icpc0928@gmail.com>
2020-06-29 20:25:02 +0800

    [20200627] 網路上他人作法...測試中

--- src/com/gameSudoku/Sudoku.java
+++ src/com/gameSudoku/Sudoku.java
@@ -16,16 +16,14 @@ public class Sudoku {
             {0, 0, 0, 0, 0, 0, 0, 0, 0, },
             {0, 0, 0, 0, 0, 0, 0, 0, 0, },
     };
-    static int limit_times = 10000;
-
-
+    static int limit_times = 100;
 
     public static void main(String[] arg){
 
         int limit_x = 0;
-        for(int x = 0; x < 9 ; x++) {
-            System.out.println("---------------"+x+"----------------");
-            inLine: for (int line = 0; line < grid[x].length; line++) {              //第一列的內文
+        for(int line = 0; line < 9 ; line++) {
+            System.out.println("---------------"+line+"----------------");
+            inLine: for (int inline = 0; inline < grid[line].length; inline++) {              //第一列的內文
                 int temp;
                 int limit_line = 0;
                 do {
@@ -33,41 +31,41 @@ public class Sudoku {
                     boolean isColSame = false;
                     boolean isRowSame = false;
                     boolean isGridSame = false;
-                    for (int col = 0; col <= line; col++) {
-                        if (temp == grid[x][col]) {
+                    for (int col = 0; col <= inline; col++) {
+                        if (temp == grid[line][col]) {
                             isColSame = true;
                             break;
                         }
                     }
                     for (int row = 0; row < grid.length; row++) {
-                        if (temp == grid[row][line] && row != x) {
+                        if (temp == grid[row][inline] && row != line) {
                             isRowSame = true;  //可能會遇到自己
                             break;
                         }
                     }
-                    int[] littleGrid = {x / 3, line / 3};
+                    int[] littleGrid = {line / 3, inline / 3};
                     for (int i = 0; i < 3; i++) {
                         if (temp == grid[littleGrid[0] * 3    ][i + littleGrid[1] * 3] ) isGridSame = true;   //要避免遇到自己
                         if (temp == grid[littleGrid[0] * 3 + 1][i + littleGrid[1] * 3] ) isGridSame = true;   //要避免遇到自己
                         if (temp == grid[littleGrid[0] * 3 + 2][i + littleGrid[1] * 3] ) isGridSame = true;   //要避免遇到自己
                     }
                     if(!isColSame && !isGridSame && !isRowSame){
-                        grid[x][line] = temp;
+                        grid[line][inline] = temp;
                     }
                     limit_line++;
-                } while (grid[x][line] == 0 && limit_line < limit_times );
+                } while (grid[line][inline] == 0 && limit_line < limit_times );
 
-                if(limit_line == limit_times){      //極限重製_1  重算該行
+                if(limit_line == limit_times){      //重製_1  重算該行
                     for(int i = 0; i < 9; i++){     //清空該行
-                        grid[x][i] = 0;
+                        grid[line][i] = 0;
                     }
-                    x--;
+                    line--;
                     limit_x++;
 
                     break inLine;
                 }
-                if(limit_x == 10 ){                  //極限重製_2 表格重算
-                    x = -1;
+                if(limit_x == limit_times ){                  //重製_2 表格重算
+                    line = -1;
                     limit_x = 0;
                     for(int i = 0; i < 9; i++){     //清空所有數據
                         for(int j = 0; j < 9; j++){
